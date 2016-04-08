efb63da3c8c8ce26ec04a1c6ba678447155794f5
Scott Colby <scolby33@gmail.com>
2016-04-08 14:21:54 -0700

    Add some more constants for test messages

--- tests/constants.py
+++ tests/constants.py
@@ -1,3 +1,8 @@
 TEST_TOKEN = 'azGDORePK8gMaC0QOYAMyEEuzJnyUi'
 TEST_USER = 'uQiRzpo4DXghDmr9QzzfQu27cmVRsG'
 TEST_DEVICE = 'droid2'
+TEST_TITLE = 'Backup finished - SQL1'
+TEST_MESSAGE = 'Backup of database "example" finished in 16 minutes.'
+TEST_REQUEST_ID = 'e460545a8b333d0da2f3602aff3133d6'
+
+PUSHOVER_API_URL = 'https://api.pushover.net/1/'
