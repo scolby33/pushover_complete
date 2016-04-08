db595d32856755986b33363c00b00b0b35b90efd
Scott Colby <scolby33@gmail.com>
2016-04-08 14:58:57 -0700

    Implement get_sounds
    Also switch to composing urls from the base API url

--- src/pushover_complete/pushover_api.py
+++ src/pushover_complete/pushover_api.py
@@ -1,16 +1,22 @@
+from urllib.parse import urljoin
+
 import requests
 
 from .error import PushoverCompleteError
+
+PUSHOVER_API_URL = 'https://api.pushover.net/1/'
+
 
 class PushoverAPI(object):
     def __init__(self, token):
         self.token = token
 
-    def send_message(self, user, message, device=None, title=None, url=None, url_title=None,
-                     priority=None, retry=None, expire=None, timestamp=None, sound=None, html=False):
+    def _send_message(self, user, message, device=None, title=None, url=None, url_title=None,
+                      priority=None, retry=None, expire=None, timestamp=None, sound=None, html=False, session=None):
         if priority == 2:
             if retry is None or expire is None:
                 raise PushoverCompleteError('Must specify `retry` and `expire` with priority 2.')
+
         payload = {
             'token': self.token,
             'user': user,
@@ -27,10 +33,27 @@ class PushoverAPI(object):
             'html': html
         }
         headers = {'Content-type': 'application/x-www-form-urlencoded'}
-        r = requests.post(
-            'https://api.pushover.net/1/messages.json',
-            data=payload,
-            headers=headers
+
+        if session is None:
+            resp = requests.post(
+                urljoin(PUSHOVER_API_URL, 'messages.json'),
+                data=payload,
+                headers=headers
+            )
+        else:
+            resp = session.post(
+                urljoin(PUSHOVER_API_URL, 'messages.json'),
+                data=payload,
+                headers=headers
+            )
+        return resp
+
+    def send_message(self, user, message, device=None, title=None, url=None, url_title=None,
+                     priority=None, retry=None, expire=None, timestamp=None, sound=None, html=False):
+        resp = self._send_message(user, message, device, title, url, url_title, priority, retry, expire, timestamp,
+                                  sound, html)
+        return resp
+
     def get_sounds(self):
         resp = requests.get(
             urljoin(PUSHOVER_API_URL, 'sounds.json'),
