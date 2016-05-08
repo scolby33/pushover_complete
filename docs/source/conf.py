3ee99c37c2ac1cfcbf9451b7dff7603750893bcd
Scott Colby <scolby33@gmail.com>
2016-05-08 16:15:38 -0700

    Add intersphinx mapping for requests.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -298,6 +298,9 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'requests': ('http://docs.python-requests.org/en/master', None)
+}
 
 autodoc_member_order = 'bysource'
