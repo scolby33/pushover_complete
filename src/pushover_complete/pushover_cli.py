8a50fed2c009c3f8b813c206b45cfdcc2d9aa0a1
Scott Colby <scolby33@gmail.com>
2016-07-05 00:18:24 -0700

    Implementation and documentation of all CLI commands.

--- src/pushover_complete/pushover_cli.py
+++ src/pushover_complete/pushover_cli.py
@@ -10,9 +10,10 @@ except ImportError:
 
 import click
 
-from .pushover_api import PushoverAPI
+from .pushover_api import PushoverAPI, BadAPIRequestError
 
 NAME = 'Pushover Complete'
+MISSING_ARG = 'Required input missing: {}'
 
 
 def _get_sys_dir(app_name, force_posix=False):
@@ -37,7 +38,7 @@ def _read_configs(file_config=None):
     return config
 
 
-def _update_configs_from_args(ctx, args):
+def _update_configs_from_args(ctx, args, keys_to_remove=None):
     args = copy.copy(args)
     try:
         args.pop('ctx')
@@ -45,16 +46,23 @@ def _update_configs_from_args(ctx, args):
         pass
     args = {k: v for k, v in args.items() if v}
     ctx.obj['config'].update(args)
+    if keys_to_remove:
+        for key in keys_to_remove:
+            ctx.obj['config'].pop(key)
     # pprint(ctx.obj['config'])
 
 
 @click.group()
-@click.option('--token')
-@click.option('--config', '-c', type=click.File())
-@click.option('--preset', '-p', default='DEFAULT')
+@click.option('--token', metavar='TOKEN', help='A Pushover application token')
+@click.option('--config', '-c', type=click.File(), help='A configuration file to override system- and user-level configuration files')
+@click.option('--preset', '-p', default='DEFAULT', help='A configuration preset to apply')
 @click.version_option()
 @click.pass_context
 def cli(ctx, token, config, preset):
+    """Interact with the Pushover API via command line.
+
+    Brought to you by the `pushover_complete` Python package.
+    """
     config = _read_configs(config)
     ctx.obj = {'config': defaultdict(lambda: None, config.items(preset))}
     if token:
@@ -64,102 +72,199 @@ def cli(ctx, token, config, preset):
 
 @cli.command()
 @click.argument('message', required=False)
-@click.option('--user')
-@click.option('--device')
-@click.option('--title')
-@click.option('--url')
-@click.option('--url-title')
-@click.option('--priority')
-@click.option('--retry')
-@click.option('--expire')
-@click.option('--callback-url')
-@click.option('--timestamp')
-@click.option('--sound')
-@click.option('--html', is_flag=True)
+@click.option('--user', metavar='TOKEN', help='A Pushover user token representing the user or group to whom the message will be sent')
+@click.option('--device', help='A comma-separated string representing the device(s) to which the message will be sent')
+@click.option('--title', help='The title of the message')
+@click.option('--url', metavar='URL', help='A URL to be included with the message')
+@click.option('--url-title', help='The link text to be displayed for the URL. If omitted, the URL itself is displayed.')
+@click.option('--priority', metavar='INT', help='An integer representing the priority of the message, from -2 (least important) to 2 (emergency). Default is 0.')
+@click.option('--retry', metavar='INT', help='How often the Pushover server will re-send an emergency-priority message in seconds. Required with priority 2 messages.')
+@click.option('--expire', metavar='INT', help='How long an emergency-priority message will be re-sent for in seconds')
+@click.option('--callback-url', metavar='URL', help='A url to be visited by the Pushover servers upon acknowledgement of an emergency-priority message')
+@click.option('--timestamp', metavar='INT', help="A Unix timestamp of the message's date and time to be displayed instead of the time the message is received by the Pushover servers")
+@click.option('--sound', help="A string representing the sound to be played with the message instead of the user's default. Available sounds can be retreived using the `sounds` command.")
+@click.option('--html', is_flag=True, help='A flag representing if HTML formatting will be enabled for the message text')
 @click.pass_context
 def send(ctx, message, user, device, title, url, url_title, priority, retry, expire, callback_url, timestamp, sound, html):
-    _update_configs_from_args(ctx, locals())
+    """Send [MESSAGE] via the Pushover API."""
+    _update_configs_from_args(ctx, locals(), ('token',))
+    try:
+        ctx.obj['api'].send_message(**ctx.obj['config'])
+    except TypeError as e:
+        ctx.fail(MISSING_ARG.format(e.args[0].split(':')[1].strip()))
+    except BadAPIRequestError as e:
+        ctx.exit(e.args[0].split(':')[-1].strip())
 
 
 @cli.command()
 @click.pass_context
 def sounds(ctx):
-    sounds_list = ctx.obj['api'].get_sounds()
+    """Get the current list of supported sounds from the Pushover servers."""
+    try:
+        sounds_list = ctx.obj['api'].get_sounds()
+    except BadAPIRequestError as e:
+        ctx.exit(e.args[0].split(':')[-1].strip())
     for identifier, name in sounds_list.items():
         print('{}: {}'.format(identifier, name))
 
 
 @cli.command()
 @click.argument('user', required=False)
-@click.option('--device')
+@click.option('--device', help='A string representing a device name to validate')
 @click.pass_context
 def validate(ctx, user, device):
-    _update_configs_from_args(ctx, locals())
+    """Validate a user or group token or a user device."""
+    _update_configs_from_args(ctx, locals(), ('token',))
+    try:
+        ctx.obj['api'].validate(**ctx.obj['config'])
+    except TypeError as e:
+        ctx.fail(MISSING_ARG.format(e.args[0].split(':')[1].strip()))
+    except BadAPIRequestError as e:
+        ctx.exit(e.args[0].split(':')[-1].strip())
 
 
-@cli.command()
-@click.argument('receipt', required=False)
+
+@cli.group(invoke_without_command=True)
+@click.option('--receipt', metavar='TOKEN', help='The receipt id')
 @click.pass_context
 def receipt(ctx, receipt):
+    """Check a receipt issued after sending an emergency-priority message."""
+    _update_configs_from_args(ctx, locals(), ('token',))
+    if not ctx.invoked_subcommand:
+        try:
+            ctx.obj['api'].check_receipt(**ctx.obj['config'])
+        except TypeError as e:
+            ctx.fail(MISSING_ARG.format(e.args[0].split(':')[1].strip()))
+        except BadAPIRequestError as e:
+            ctx.exit(e.args[0].split(':')[-1].strip())
+
+
+@receipt.command(name='cancel')
+@click.pass_context
+def receipt_cancel(ctx):
+    """Cancel a receipt (and thus further re-sends of the message)."""
     _update_configs_from_args(ctx, locals())
+    try:
+        ctx.obj['api'].cancel_receipt(**ctx.obj['config'])
+    except TypeError as e:
+        ctx.fail(MISSING_ARG.format(e.args[0].split(':')[1].strip()))
+    except BadAPIRequestError as e:
+        ctx.exit(e.args[0].split(':')[-1].strip())
 
 
 @cli.command()
 @click.argument('user', required=False)
-@click.argument('subscription', required=False)
-@click.option('--device')
-@click.option('--sound')
+@click.option('--subscription-code', metavar='TOKEN', help='The subscription code to migrate the user to')
+@click.option('--device', help="The user's device that the subscription will be limited to")
+@click.option('--sound', help="The user's preferred sound")
 @click.pass_context
-def migrate(ctx, user, subscription, device, sound):
-    _update_configs_from_args(ctx, locals())
+def migrate(ctx, user, subscription_code, device, sound):
+    """Migrate [USER] to a subscription key."""
+    _update_configs_from_args(ctx, locals(), ('token',))
+    try:
+        ctx.obj['api'].migrate_to_subscription(**ctx.obj['config'])
+    except TypeError as e:
+        ctx.fail(MISSING_ARG.format(e.args[0].split(':')[1].strip()))
+    except BadAPIRequestError as e:
+        ctx.exit(e.args[0].split(':')[-1].strip())
 
 
 @cli.group(invoke_without_command=True)
-@click.option('--group-key')
+@click.option('--group-key', metavar='TOKEN', help='A Pushover group key')
 @click.pass_context
 def group(ctx, group_key):
-    _update_configs_from_args(ctx, locals())
+    """Retrieve information about a delivery group."""
+    _update_configs_from_args(ctx, locals(), ('token',))
+    if not ctx.invoked_subcommand:
+        try:
+            pprint(ctx.obj['api'].group_info(**ctx.obj['config']))
+        except TypeError as e:
+            ctx.fail(MISSING_ARG.format(e.args[0].split(':')[1].strip()))
+        except BadAPIRequestError as e:
+            ctx.exit(e.args[0].split(':')[-1].strip())
 
 
 @group.command(name='add')
 @click.argument('user', required=False)
-@click.option('--device')
-@click.option('--memo')
+@click.option('--device', help='The user key to be added to the group')
+@click.option('--memo', help='A string representing the device name to add to the group')
 @click.pass_context
 def group_add(ctx, user, device, memo):
+    """Add [USER] to a group."""
     _update_configs_from_args(ctx, locals())
+    try:
+        ctx.obj['api'].group_add_user(**ctx.obj['config'])
+    except TypeError as e:
+        ctx.fail(MISSING_ARG.format(e.args[0].split(':')[1].strip()))
+    except BadAPIRequestError as e:
+        ctx.exit(e.args[0].split(':')[-1].strip())
 
 
 @group.command(name='delete')
 @click.argument('user', required=False)
 @click.pass_context
 def group_delete(ctx, user):
+    """Remove [USER] from a group."""
     _update_configs_from_args(ctx, locals())
+    try:
+        ctx.obj['api'].group_delete_user(**ctx.obj['config'])
+    except TypeError as e:
+        ctx.fail(MISSING_ARG.format(e.args[0].split(':')[1].strip()))
+    except BadAPIRequestError as e:
+        ctx.exit(e.args[0].split(':')[-1].strip())
 
 
 @group.command(name='disable')
 @click.argument('user', required=False)
 @click.pass_context
 def group_disable(ctx, user):
+    """Temporarily disable [USER] in a group."""
     _update_configs_from_args(ctx, locals())
+    try:
+        ctx.obj['api'].group_disable_user(**ctx.obj['config'])
+    except TypeError as e:
+        ctx.fail(MISSING_ARG.format(e.args[0].split(':')[1].strip()))
+    except BadAPIRequestError as e:
+        ctx.exit(e.args[0].split(':')[-1].strip())
 
 
 @group.command(name='enable')
 @click.argument('user', required=False)
 @click.pass_context
 def group_enable(ctx, user):
+    """Re-enable [USER] in a group."""
     _update_configs_from_args(ctx, locals())
+    try:
+        ctx.obj['api'].group_enable_user(**ctx.obj['config'])
+    except TypeError as e:
+        ctx.fail(MISSING_ARG.format(e.args[0].split(':')[1].strip()))
+    except BadAPIRequestError as e:
+        ctx.exit(e.args[0].split(':')[-1].strip())
 
 
 @group.command(name='rename')
 @click.argument('new_name', required=False)
 @click.pass_context
 def group_rename(ctx, new_name):
+    """Change the name of a group to [NEW_NAME]."""
     _update_configs_from_args(ctx, locals())
+    try:
+        ctx.obj['api'].group_rename(**ctx.obj['config'])
+    except TypeError as e:
+        ctx.fail(MISSING_ARG.format(e.args[0].split(':')[1].strip()))
+    except BadAPIRequestError as e:
+        ctx.exit(e.args[0].split(':')[-1].strip())
 
 
 @cli.command()
 @click.argument('user', required=False)
 @click.pass_context
 def assign(ctx, user):
-    _update_configs_from_args(ctx, locals())
+    """Assign a Pushover license to [USER]."""
+    _update_configs_from_args(ctx, locals(), ('token',))
+    try:
+        ctx.obj['api'].assign_license(**ctx.obj['config'])
+    except TypeError as e:
+        ctx.fail(MISSING_ARG.format(e.args[0].split(':')[1].strip()))
+    except BadAPIRequestError as e:
+        ctx.exit(e.args[0].split(':')[-1].strip())
