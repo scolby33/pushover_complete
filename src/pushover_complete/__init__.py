f3071cfe2cf5039f14d2f96089adbc6c15dd3cf7
Scott Colby <scolby33@gmail.com>
2018-04-06 21:26:07 -0700

    Bump version: 1.0.3-dev → 1.1.0

--- src/pushover_complete/__init__.py
+++ src/pushover_complete/__init__.py
@@ -8,7 +8,7 @@ __all__ = [
     'PushoverAPI'
 ]
 
-__version__ = '1.0.3-dev'
+__version__ = '1.1.0'
 
 __title__ = 'pushover_complete'
 __description__ = 'A Python package for interacting with *all* aspects of the Pushover API'
