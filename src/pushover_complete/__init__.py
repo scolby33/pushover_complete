062d725105c9c8d5c52876617831c57b61c83a26
Scott Colby <scott@scolby.com>
2024-07-04 18:21:05 -0400

    Replace setup.py with pyproject.toml.

--- src/pushover_complete/__init__.py
+++ src/pushover_complete/__init__.py
@@ -10,14 +10,5 @@ __all__ = [
 
 __version__ = '1.1.2-dev'
 
-__title__ = 'pushover_complete'
-__description__ = 'A Python package for interacting with *all* aspects of the Pushover API'
-__url__ = 'https://github.com/scolby33/pushover_complete'
-
-
-__author__ = 'Scott Colby'
-__email__ = 'scolby33@gmail.com'
-
-
 __license__ = 'MIT License'
 __copyright__ = 'Copyright (c) 2016 Scott Colby'
